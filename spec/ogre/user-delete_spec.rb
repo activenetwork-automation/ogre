f0b7882d7ac8570847babbdf52c7a98f630556f9
Joe Nguyen <joe.nguyen@activenetwork.com>
2015-04-27 15:23:29 -0500

    init

---
+++ spec/ogre/user-delete_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../spec_helper.rb'
+require 'ogre'
+
+# rubocop:disable LineLength
+
+describe Ogre::UserDelete do
+
+  VCR.configure do |config|
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+    config.hook_into :webmock
+
+    # remove sensitive authentication information from the recording
+    config.before_record do |interaction|
+      headers = interaction.request.headers
+      headers.keys.
+        select { |k| k =~ /^X-Ops-(Authorization-|Content-Hash)/ }.
+        each { |header| headers[header] = Array("{{#{header}}}") }
+      headers["X-Ops-Userid"] = "pivotal"
+    end
+  end
+
+  it 'delete user' do
+    args = %w(user -f --run_as pivotal --key_path spec/fixtures/client_key/dummy.pem --server_url https://chef.server)
+    VCR.use_cassette("user-delete") do
+      options = Ogre::UserDelete.start(args)
+    end
+  end
+end
+
+#rubocop:enable LineLength

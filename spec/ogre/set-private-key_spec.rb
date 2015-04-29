f98c61650b698ecb96a0af73a28cbae1c936717f
Joe Nguyen <joe.nguyen@activenetwork.com>
2015-04-29 14:36:20 -0500

    refactored to use expect

--- spec/ogre/set-private-key_spec.rb
+++ spec/ogre/set-private-key_spec.rb
@@ -9,11 +9,17 @@ VCR.configure do |config|
 end
 
 describe Ogre::SetPrivateKey do
-
   it 'should set private key via vco workflow' do
     args = %w(chef.server org-validator spec/fixtures/client_key/dummy.pem) + VCO_DEFAULTS
+    response = "Execution ID:      some-long-execution-id\n" \
+               "Name:              Set-Private-Key\n" \
+               "Workflow ID:       some-long-workflow-id\n" \
+               "State:             completed\n" \
+               "Start Date:        2015-04-27 14:40:01 -0500\n" \
+               "End Date:          2015-04-27 14:40:01 -0500\n" \
+               "Started By:        user@example.com\n"
     VCR.use_cassette('set-private-key', match_requests_on: [:uri]) do
-      Ogre::SetPrivateKey.start(args)
+      expect { Ogre::SetPrivateKey.start(args) }.to output(response).to_stdout
     end
   end
 

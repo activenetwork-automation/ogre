886f7c2081c63a71704e884018d396bc7cd5ba86
Joe Nguyen <joe.nguyen@activenetwork.com>
2015-09-01 09:51:46 -0500

    fix spec tests

--- spec/ogre/set-private-key_spec.rb
+++ spec/ogre/set-private-key_spec.rb
@@ -7,6 +7,22 @@ VCR.configure do |config|
 end
 
 describe Ogre::SetPrivateKey do
+  before(:each) do
+    @url = 'https://vco.server:8281/'
+    @username = 'user'
+    @password = 'password'
+
+    @config_file = "#{ENV['HOME']}/.vcoworkflows/config.json"
+    @config_data = {
+      url: @url,
+      username: @username,
+      password: @password
+    }.to_json
+
+    allow(File).to receive(:read).and_call_original
+    allow(File).to receive(:read).with(@config_file).and_return(@config_data)
+  end
+
   it 'should set private key via vco workflow' do
     args = %w(chef.server org-validator spec/fixtures/client_key/dummy.pem) + VCO_DEFAULTS
     VCR.use_cassette('set-private-key', match_requests_on: [:uri]) do
